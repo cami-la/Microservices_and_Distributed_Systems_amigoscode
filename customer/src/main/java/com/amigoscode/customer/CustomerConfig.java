462bd63d8c521e79f43fde38021c4a74ae69968b
Camila Cavalcante <camiladsantoscavalcante@gmail.com>
2023-01-22 07:26:00 -0300

    Testing Microservice Communication

--- customer/src/main/java/com/amigoscode/customer/CustomerConfig.java
+++ customer/src/main/java/com/amigoscode/customer/CustomerConfig.java
@@ -5,7 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
-public record CustomerConfig() {
+public class CustomerConfig {
   @Bean
   public RestTemplate restTemplate() {
     return new RestTemplate();
--- fraud/src/main/java/com/amigoscode/fraud/FraudController.java
+++ fraud/src/main/java/com/amigoscode/fraud/FraudController.java
@@ -1,9 +1,11 @@
 package com.amigoscode.fraud;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 
 @RequestMapping("/api/fraud-check")
 @RestController
+@Slf4j
 public record FraudController(
     FraudCheckService fraudCheckService
 ) {
@@ -11,6 +13,7 @@ public record FraudController(
   @GetMapping(path="{customerId}")
   public FraudCheckResponse isFraudster(@PathVariable("customerId") Integer customerId) {
     boolean isFraudulentCustomer = this.fraudCheckService.isFraudulentCustomer(customerId);
+    log.info("fraud check request for customer {}", customerId);
     return new FraudCheckResponse(isFraudulentCustomer);
   }
 }

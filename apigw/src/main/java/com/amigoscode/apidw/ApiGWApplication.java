74914db0c7007f5209d339397013fcaaf95d4c09
Camila Cavalcante <camiladsantoscavalcante@gmail.com>
2023-02-01 18:51:46 -0300

    Bootstrap API Gateway

---
+++ apigw/src/main/java/com/amigoscode/apidw/ApiGWApplication.java
@@ -0,0 +1,12 @@
+package com.amigoscode.apidw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGWApplication {
+  public static void main(String[] args) {
+
+    SpringApplication.run(ApiGWApplication.class, args);
+  }
+}
